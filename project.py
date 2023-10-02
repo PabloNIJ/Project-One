a364e70651b4db578f84bf0154ec22690215f003
PabloNIJ <145595397+PabloNIJ@users.noreply.github.com>
2023-10-02 14:29:30 +0100

    Update project.py

--- project.py
+++ project.py
@@ -1,19 +1,14 @@
 import streamlit as st
+import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
-from sklearn.naive_bayes import GaussianNB
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-gnb = GaussianNB()
-
-Tfidf = TfidfVectorizer()
 
 ps = PorterStemmer()
 
 
-def transform_text(text):
+def transform_text(text):   
     text = text.lower()
     text = nltk.word_tokenize(text)
 
@@ -38,8 +33,8 @@ def transform_text(text):
     return " ".join(y)
 
 
-#Tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
-#model = pickle.load(open('model.pkl', 'rb'))
+Tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 st.title("Email/SMS Spam Classifier")
 
@@ -50,9 +45,9 @@ if st.button('Predict'):
     # 1. preprocess
     transformed_sms = transform_text(input_sms)
     # 2. vectorize
-    vector_input = Tfidf.fit_transform([transformed_sms])
+    vector_input = Tfidf.transform([transformed_sms])
     # 3. predict
-    result = gnb.predict(vector_input)
+    result = model.predict(vector_input)
     # 4. Display
     if result == 1:
         st.header("Spam")
